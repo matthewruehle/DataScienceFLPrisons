0dee7276590dc3402862eff653d71841239e5cd3
Matt Ruehle <matthew.t.ruehle@gmail.com>
2017-01-27 16:30:09 -0500

    Reading from dataset.

---
+++ read_FL_database_into_pandas.py
@@ -0,0 +1,20 @@
+# Does what it says on the tin.
+# Notoe that it takes a while to read all the mdb thingies.
+# -Matt
+
+import pandas
+import numpy as np
+import pandas_access
+
+DATASET = "./FDOC_January_2017.mdb"
+
+if __name__ == "__main__":
+	table_list = []
+	for tablename in pandas_access.list_tables(DATASET):
+		print(tablename)
+		table_list.append(tablename)
+
+	tables_dict = {}
+		
+	for table in table_list[0:2]: #doing more makes it take forever.
+		tables_dict[table] = pandas_access.read_table(DATASET, table)
